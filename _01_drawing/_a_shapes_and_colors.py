d3fd6baa1de0b1d79b16a3a15f03df94d93256b6
root <root@root.com>
2020-11-23 22:24:13 +0000

    day 2

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,23 +5,27 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # This code makes a new Turtle. Pick a new name for the turtle
-    myTurtle = turtle.Turtle()
+    Lilly = turtle.Turtle()
     
     # Make your turtle's shape 'turtle', .shape('turtle')
-    
+    Lilly.shape("turtle")
     # Set your turtle's speed using .speed(2)
-    
+    Lilly.speed( 3 )
     # Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    Lilly.color("pink")
     # Move your turtle forward using .forward(100)
+    Lilly.forward(100)
     # TEST    Did your turtle move forward?
-    
     # Move your turtle left or right using .left(90) or .right(90)
-    
+    Lilly.left(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
+
+    for i in range(8):
+        Lilly.forward(100)
+        Lilly.left(90)
     # TEST    Did your turtle draw a square?
-        
     # Move your turtle to a new place on the screen using .goto(x, y)
+    Lilly
     # x=0 and y=0 is the center of the screen
     
     # Have your turtle draw a circle using .circle(radius, steps=30)
